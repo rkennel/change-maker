30d92dc0165823276a752a567c95fd4d7901bc12
Ryan Kennel <rkennel@ford.com>
2019-02-09 14:29:52 -0500

    refactored makeChangeAddCoins method to take Change as input

--- src/main/kotlin/Change.kt
+++ src/main/kotlin/Change.kt
@@ -1,8 +1,8 @@
 data class Change(
-    val pennies: Int,
-    val nickels: Int,
-    val dimes: Int,
-    val quarters: Int
+    val pennies: Int = 0,
+    val nickels: Int = 0,
+    val dimes: Int = 0,
+    val quarters: Int = 0
 )
 
 enum class Coin(val valueInPennies:Int){
@@ -14,38 +14,36 @@ enum class Coin(val valueInPennies:Int){
 }
 
 fun makeChange(changeToMake: Int): List<Change> {
-    return makeChangeAddCoins(changeToMake,0,0,0,0,Coin.QUARTER)
+    return makeChangeAddCoins(changeToMake,Change(),Coin.QUARTER)
 }
 
 private fun makeChangeAddCoins(
     changeToMake: Int,
-    numberOfQuarters: Int,
-    numberOfDimes: Int,
-    numberOfNickels: Int,
-    numberOfPennies: Int,
+    currentChange: Change,
     coin: Coin
 ): List<Change> {
+
+    if(coin==Coin.PENNY){
+        return listOf(Change(changeToMake,currentChange.nickels,currentChange.dimes,currentChange.quarters))
+    }
+
     val changeCombinations = mutableListOf<Change>()
 
     val maxCoins = changeToMake / coin.valueInPennies;
 
     for (i in 0..maxCoins) {
 
-        val q = if(coin==Coin.QUARTER) i else numberOfQuarters
-        val d = if(coin==Coin.DIME) i else numberOfDimes
-        val n = if(coin==Coin.NICKEL) i else numberOfNickels
-
-        val nextCoin = determineNextCoin(coin)
+        val updatedChange = Change(
+            0,
+            if(coin==Coin.NICKEL) i else currentChange.nickels,
+            if(coin==Coin.DIME) i else currentChange.dimes,
+            if(coin==Coin.QUARTER) i else currentChange.quarters
+        )
 
         val valueInCoins = i * coin.valueInPennies
         val remainingChangeToMake = changeToMake - valueInCoins
 
-        if(nextCoin==Coin.NONE){
-            changeCombinations.add(Change(remainingChangeToMake,n,d,q))
-        }
-        else{
-            changeCombinations.addAll(makeChangeAddCoins(remainingChangeToMake,q,d,n,numberOfPennies,nextCoin))
-        }
+        changeCombinations.addAll(makeChangeAddCoins(remainingChangeToMake,updatedChange,determineNextCoin(coin)))
     }
 
     return changeCombinations
@@ -55,6 +53,7 @@ private fun determineNextCoin(coin: Coin): Coin {
     return when (coin) {
         Coin.QUARTER -> Coin.DIME
         Coin.DIME -> Coin.NICKEL
+        Coin.NICKEL -> Coin.PENNY
         else -> Coin.NONE
     }
 }
