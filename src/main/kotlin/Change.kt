55f8853090a9274a46e9e181e41fc765ea554c8d
Ryan Kennel <rkennel@ford.com>
2019-02-08 19:51:01 -0500

    Added Quarters

--- src/main/kotlin/Change.kt
+++ src/main/kotlin/Change.kt
@@ -6,38 +6,53 @@ data class Change(
 )
 
 fun makeChange(changeToMake: Int): List<Change> {
-    return makeChangeAddDimes(changeToMake)
+    return makeChangeAddQuarters(changeToMake)
 }
 
-fun makeChangeAddDimes(changeToMake: Int): List<Change> {
+private fun makeChangeAddQuarters(changeToMake: Int): List<Change> {
     val changeCombinations = mutableListOf<Change>()
 
-    val numberOfPenniesInADime = 10
-    val maxDimes = changeToMake / numberOfPenniesInADime;
+    val numberOfPenniesInAQuarter = 25
+    val maxQuarters = changeToMake / numberOfPenniesInAQuarter;
 
-    for (i in 0..maxDimes){
-        val valueInDimes = i * numberOfPenniesInADime
-        changeCombinations.addAll(makeChangeAddNickels(i,changeToMake- valueInDimes))
+    for (i in 0..maxQuarters) {
+        val valueInQuarters = i * numberOfPenniesInAQuarter
+        changeCombinations.addAll(makeChangeAddDimes(i, changeToMake - valueInQuarters))
     }
 
     return changeCombinations
 }
 
-private fun makeChangeAddNickels(numberOfDimes:Int,changeToMake: Int): List<Change> {
+private fun makeChangeAddDimes(numberOfQuarters:Int,changeToMake: Int): List<Change> {
+    val changeCombinations = mutableListOf<Change>()
+
+    val numberOfPenniesInADime = 10
+    val maxDimes = changeToMake / numberOfPenniesInADime;
+
+    for (i in 0..maxDimes) {
+        val valueInDimes = i * numberOfPenniesInADime
+        changeCombinations.addAll(makeChangeAddNickels(numberOfQuarters, i, changeToMake - valueInDimes))
+    }
+
+    return changeCombinations
+}
+
+private fun makeChangeAddNickels(numberOfQuarters: Int,numberOfDimes: Int, changeToMake: Int): List<Change> {
 
     val changeCombinations = mutableListOf<Change>()
 
     val numberOfPenniesInANickel = 5
     val maxNickels = changeToMake / numberOfPenniesInANickel;
 
-    for (i in 0..maxNickels){
+    for (i in 0..maxNickels) {
         val valueInNickles = i * numberOfPenniesInANickel
-        changeCombinations.add(makeChangeAddPennies(numberOfDimes, i,changeToMake- valueInNickles))
+        changeCombinations.addAll(makeChangeAddPennies(numberOfQuarters, numberOfDimes, i, changeToMake - valueInNickles))
     }
 
     return changeCombinations
 }
 
-private fun makeChangeAddPennies(numberOfDimes:Int, numberOfNickels: Int, changeToMake: Int): Change {
-    return Change(changeToMake, numberOfNickels, numberOfDimes, 0)
+private fun makeChangeAddPennies(numberOfQuarters: Int,numberOfDimes: Int, numberOfNickels: Int, changeToMake: Int): List<Change> {
+    return mutableListOf(Change(changeToMake, numberOfNickels, numberOfDimes, numberOfQuarters))
 }
+
--- src/test/kotlin/ChangeTest.kt
+++ src/test/kotlin/ChangeTest.kt
@@ -51,5 +51,33 @@ class ChangeTest {
         assertThat(actual).isEqualTo(expected)
     }
 
+    @Test
+    fun when26Cents13Combinations(){
+        //setup
+        val expected = listOf(
+            Change(26,0,0,0),
+            Change(21,1,0,0),
+            Change(16,2,0,0),
+            Change(16,0,1,0),
+            Change(11,3,0,0),
+            Change(11,1,1,0),
+            Change(6,4,0,0),
+            Change(6,2,1,0),
+            Change(6,0,2,0),
+            Change(1,5,0,0),
+            Change(1,3,1,0),
+            Change(1,1,2,0),
+            Change(1,0,0,1)
+        )
+
+        //act
+        val actual = makeChange(26)
+
+        //assert
+        assertThat(actual).containsAll(expected)
+        assertThat(expected).containsAll(actual)
+
+    }
+
 
 }
